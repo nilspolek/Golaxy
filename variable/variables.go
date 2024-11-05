7cf491655cc9bbf3bc8dae678fb128c9af5a59ba
nilspolek <nils@polek.de>
2024-11-05 19:40:45 +0100

    added variables that get renderd

---
+++ variable/variables.go
@@ -0,0 +1,25 @@
+package variables
+
+import (
+	"github.com/nilspolek/Golaxy/router"
+)
+
+type Variable struct {
+	value interface{}
+	r     *router.Router
+}
+
+func New(r *router.Router) *Variable {
+	return &Variable{
+		r: r,
+	}
+}
+
+func (v *Variable) Set(val interface{}) {
+	v.value = val
+	v.r.Render()
+}
+
+func (v *Variable) Get() interface{} {
+	return v.value
+}
